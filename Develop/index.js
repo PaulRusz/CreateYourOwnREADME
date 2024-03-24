d08a0597cd267b6c740f629a3303dd4a0a9402a3
PaulRusz <paulruszkay@gmail.com>
2024-03-24 14:17:37 -0400

    added index.js code & README

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,121 @@
-// TODO: Include packages needed for this application
+// install inquirer8.2.4
+// install colors
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const generateREADME = ({ title, repoLink, deployedLink, description, installation, usage, license, author, tests, username, email }) =>
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
+
+    `# ${title}
+
+## Description\n
+${description}
+
+## Table of Contents\n
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation\n
+${installation}
+
+## Usage\n
+${usage}
+
+## License\n
+This project is licensed under the ${license} license.
+
+## Contributing\n
+${author}
+
+## Tests\n
+${tests}
+
+## Questions\n
+For questions about this project, please contact me by either option below:\n
+GitHub: [${username}](https://github.com/${username})\n
+Email: ${email}.\n
+
+## Repository Link\n
+Here is the repository link for the application: ${repoLink}
+
+## Deployed Link\n
+Here is the link to the application: ${deployedLink}
+
+`;
+
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the Title of your project?",
+        },
+        {
+            type: "input",
+            name: "repoLink",
+            message: "What is the link to your GitHub repository?",
+        },
+        {
+            type: "input",
+            name: "deployedLink",
+            message: "What is the link to your deployed and active application?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "What is a description of your application?",
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Please provide instructions for installation of your application.",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Please provide instructions on how to operate your application.",
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Choose a license to include on the README:",
+            choices: ["Apache License - see License file for details", "ISC License - see License file for details",
+                "MIT License - see License file for details", "GNU General Public License - see License file for details"]
+        },
+        {
+            type: "input",
+            name: "author",
+            message: "Who contributed to the application and can be considered the 'author'?",
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Were any tests completed on the application to show proper functionality?",
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please provide your GitHub username so a user could reach out if they had any questions.",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please provide your email so a user could reach out if they had any questions.",
+        },
+
+    ])
+
+    .then((answers) => {
+        const READMEContent = generateREADME(answers);
+
+        fs.writeFile('README.md', READMEContent, (err) =>
+            err ? console.log(err) : console.log('Successfully created your README')
+        );
+    });
--- README.md
+++ README.md
@@ -1,3 +1,42 @@
-# Professional README Generator Starter Code
+# Title
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+Build Your Own README File
+
+## Description
+
+The Build Your Own README is a README generator.  The generator was created as a way for users to create README's easily and for the user to save time.
+
+## Installation
+
+Installation is easy! Simply use use the instructions below to add inquirer8.2.4 & colors.
+Instructions:
+1. In your Integrated Terminal type:
+  npm install inquirer@8.2.4
+  npm install colors
+
+2. To access the README creator simply type 'node index.js' in your Integrated Terminal.
+
+3. Follow the prompts in the terminal.
+
+
+## Usage
+
+The Build Your Own README can be loaded via any Integrated Terminal on VS Code. Once the terminal is open, the user should install Inquirer8.2.4 & colors as stated above. After, the user should use 'node index.js' to begin the process of creating their own README.
+After pressing the blue 'Search City' button, the current weather as well as the 5 Day Forecast will be loaded onto the webpage.
+
+The user will be prompted to complete multiple questions and choices that will be used to create a README.
+
+
+## Credits
+
+N/A
+
+## Links
+
+https://github.com/PaulRusz/BuildYourOwnREADME
+
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
